9ae2d3693265e5af7b3a5720d0356d58711d23ba
chris-innago <chris@innago.com>
2025-05-26 16:03:16 -0400

    Removes string enum converter and adds source generator tests.

---
+++ tests/UnitTests/Publisher.Amqp/SourceGeneratorContextTests.cs
@@ -0,0 +1,38 @@
+namespace UnitTests.Publisher.Amqp;
+
+using System.Text.Json;
+
+using FluentAssertions;
+
+using Innago.Platform.Messaging.EntityEvents;
+using Innago.Platform.Messaging.Publisher.Amqp;
+
+using Xunit.OpenCategories;
+
+[UnitTest(nameof(SourceGeneratorContext))]
+public class SourceGeneratorContextTests
+{
+    [Fact]
+    public void DateTimeOffsetShouldBeSetup()
+    {
+        SourceGeneratorContext.Default.GetTypeInfo(typeof(DateTimeOffset)).Should().NotBeNull();
+    }
+
+    [Fact]
+    public void DictionaryStringObjectShouldBeSetup()
+    {
+        SourceGeneratorContext.Default.GetTypeInfo(typeof(Dictionary<string, object>)).Should().NotBeNull();
+    }
+
+    [Fact]
+    public void UriShouldBeSetup()
+    {
+        SourceGeneratorContext.Default.GetTypeInfo(typeof(Uri)).Should().NotBeNull();
+    }
+
+    [Fact]
+    public void EnumsShouldSerializeAsStrings()
+    {
+        SourceGeneratorContext.Default.Options.PropertyNamingPolicy.Should().Be(JsonNamingPolicy.CamelCase);
+    }
+}
